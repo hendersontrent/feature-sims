7b83f94d1f19fb9881053370fab5c6cb678a25d2
Trent Henderson <trenthenderson@vlan-2634-10-16-141-96.student.wireless.sydney.edu.au>
2021-04-08 17:51:35 +1000

    Initial working pipeline + graphics

--- simulations/calculate_features.R
+++ simulations/calculate_features.R
@@ -10,7 +10,7 @@
 # Author: Trent Henderson, 8 April 2021
 #--------------------------------------
 
-source("setup.R") # This automatically loads the simulation function from the "/R" folder
+source("setup.R") # This automatically loads the simulation functions from the "/R" folder
 
 #----------- Simulate the processes -------------
 
@@ -44,38 +44,77 @@ calculate_comptimes <- function(){
 }
 
 comptimes <- calculate_comptimes()
-save(comptimes, "data/comptimes.Rda") # Store as .Rda
+save(comptimes, file = "data/comptimes.Rda") # Store as .Rda
 
 #----------- Visualise performance --------------
 
-plot_performance <- function(data, ratio = FALSE){
+plot_performance <- function(data, ratio = FALSE, log = FALSE){
+  
+  tmp <- data
   
   if(ratio){
-    x
-  } else{
-    x
+    
+    # Extract number of features in each set
+    
+    total_feats <- theft::feature_list %>%
+      group_by(feature_set) %>%
+      summarise(num_feats = n()) %>%
+      ungroup()
+    
+    # Calculate ratio
+    
+    tmp <- tmp %>%
+      left_join(total_feats, by = c("feature_set" = "feature_set")) %>%
+      mutate(comp_time_per_feature = comp_time / num_feats)
   }
   
-  p <- x %>%
-    ggplot(aes(x = ts_length, y = calc_time, group = process)) +
-    geom_smooth(formula = y ~ x, method = "lm") +
-    geom_point(aes(shape = process)) +
+  if(log){
+    if(ratio){
+      p <- tmp %>%
+        ggplot(aes(x = log(ts_length), y = comp_time_per_feature, group = feature_set)) +
+        labs(x = "log(Time-series Length)",
+             y = "Computational Time per Feature")
+    } else{
+      p <- tmp %>%
+        ggplot(aes(x = log(ts_length), y = comp_time, group = feature_set)) +
+        labs(x = "log(Time-series Length)",
+             y = "Computational Time")
+    }
+  } else{
+    if(ratio){
+      p <- tmp %>%
+        ggplot(aes(x = ts_length, y = comp_time_per_feature, group = feature_set)) +
+        labs(x = "Time-series Length",
+             y = "Computational Time per Feature")
+    } else{
+      p <- tmp %>%
+        ggplot(aes(x = ts_length, y = comp_time, group = feature_set)) +
+        labs(x = "Time-series Length",
+             y = "Computational Time")
+    }
+  }
+  
+  p <- p +
+    geom_point(aes(shape = feature_set)) +
     labs(title = "Performance of each feature set",
-         x = "Time-series Length") +
+         shape = "Feature Set") +
     theme_bw() +
     theme(legend.position = "bottom",
-          panel.grid.minor = element_blank())
-  
-  if(ratio){
-    p <- p +
-      labs(y = "Computational Time (s) per Feature")
-  } else{
-    p <- p +
-      labs(y = "Computational Time (s)")
-  }
+          panel.grid.minor = element_blank()) +
+    facet_wrap(~process, dir = "v")
   
   return(p)
 }
 
-overall <- plot_performance(data = outs, ratio = FALSE)
-ratio <- plot_performance(data = outs, ratio = TRUE)
+p <- plot_performance(data = comptimes, ratio = FALSE, log = FALSE)
+p1 <- plot_performance(data = comptimes, ratio = TRUE, log = FALSE)
+
+p2 <- plot_performance(data = comptimes, ratio = FALSE, log = TRUE)
+p3 <- plot_performance(data = comptimes, ratio = TRUE, log = TRUE)
+
+# Save plots
+
+ggsave(filename = "output/comptime.png", plot = p)
+ggsave(filename = "output/comptime-ratio.png", plot = p1)
+ggsave(filename = "output/comptime-log.png", plot = p2)
+ggsave(filename = "output/comptime-ratio-log.png", plot = p3)
